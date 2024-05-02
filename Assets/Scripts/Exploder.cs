66960a777a4d28fd525ddeb80841409fb334cf2a
Nikita <toropovnik1802@gmail.com>
2024-05-02 18:18:15 +0300

    cubes 2.0

--- Assets/Scripts/Exploder.cs
+++ Assets/Scripts/Exploder.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 
 public class Exploder : MonoBehaviour
 {
-    [SerializeField] private float _explosionRadius;
     [SerializeField] private float _explosionForce;
+    [SerializeField] public float _explosionRadius;
 
     public event Action<Cube> Exploded;
 
@@ -24,5 +24,40 @@ public class Exploder : MonoBehaviour
                 }
             }
         }
+    }
+
+    public void Explode(Transform cube)
+    {
+        Vector3 explosionPos = cube.position;
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, CalculateExplosionRadius(cube));
+
+        foreach (Collider nearbyObject in colliders)
+        {
+            ApplyExplosionForce(nearbyObject.transform, explosionPos);
+        }
+    }
+
+    public void ApplyExplosionForce(Transform cubeTransform, Vector3 explosionPos)
+    {
+        Rigidbody cubeRigidbody = cubeTransform.GetComponent<Rigidbody>();
+
+        if (cubeRigidbody != null)
+        {
+            cubeRigidbody.AddExplosionForce(CalculateExplosionForce(cubeTransform), explosionPos, CalculateExplosionRadius(cubeTransform));
+        }
+    }
+
+    private float CalculateExplosionForce(Transform cube) => CalculateExplosionParameter(cube, _explosionForce);
+
+    private float CalculateExplosionRadius(Transform cube) => CalculateExplosionParameter(cube, _explosionRadius);
+
+    private float CalculateExplosionParameter(Transform cubeTransform, float parameter)
+    {
+        Vector3 cubeScale = cubeTransform.localScale;
+        float cubeSize = Mathf.Max(cubeScale.x, cubeScale.y, cubeScale.z);
+
+        float result = parameter / cubeSize;
+
+        return result;
     }
 }
--- Assets/Scripts/Splitter.cs
+++ Assets/Scripts/Splitter.cs
@@ -27,41 +27,33 @@ public class Splitter : MonoBehaviour
     private void OnExploded(Cube cube)
     {
         if (_cube == cube)
-            Split(cube.transform);
+        {
+            if(CanSplit())
+                Split(cube.transform);
+            else
+                _exploder.Explode(cube.transform);
+        }
     }
 
     private void Split(Transform cube)
     {
         int numberOfCubees = Random.Range(_minCubes, _maxCubes);
 
-        if (CanSplit())
-        {
-            Vector3 cubeScale = cube.localScale / _scaleReduce;
-            float currentChanñeOfSplitting = _cube.ChanñeOfSplitting / _chanceReduce;
+        Vector3 cubeScale = cube.localScale / _scaleReduce;
+        float currentChanñeOfSplitting = _cube.ChanñeOfSplitting / _chanceReduce;
 
-            for (int i = 0; i <= numberOfCubees; i++)
-            {
-                var newCube = Instantiate(_cube, cube.position, cube.rotation);
-                newCube.Initialize(cubeScale, currentChanñeOfSplitting);
-                ApplyExplosionForce(newCube.transform);
-            }
+        for (int i = 0; i <= numberOfCubees; i++)
+        {
+            Cube newCube = Instantiate(_cube, cube.position, cube.rotation);
+            newCube.Initialize(cubeScale, currentChanñeOfSplitting);
+           _exploder.ApplyExplosionForce(newCube.transform, cube.transform.position);
         }
     }
 
     private bool CanSplit()
     {
         var chance = Random.Range(_minChanceOfSplitting, _maxChanceOfSplitting);
-        
+
         return _cube.ChanñeOfSplitting > chance ? true : false;
-    }
-
-    private void ApplyExplosionForce(Transform cubeTransform)
-    {
-        Rigidbody cubeRigidbody = cubeTransform.GetComponent<Rigidbody>();
-
-        if(cubeRigidbody != null)
-        {
-            cubeRigidbody.AddExplosionForce(1200f, cubeTransform.position, 20f);
-        }
     }
 }
